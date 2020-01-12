a50ca4dd0314d65d6de729b566e7edce04e39a7a
Collin Erickson <collinberickson@gmail.com>
2020-01-12 10:33:11 -0500

    fixing

--- R/adaptconcept2_sFFLHD_R6.R
+++ R/adaptconcept2_sFFLHD_R6.R
@@ -27,8 +27,24 @@
 #' @examples
 #' a <- adapt.concept2.sFFLHD.R6$new(D=2,L=3,func=TestFunctions::gaussian1,obj="desirability",
 #'     des_func=des_func_relmax, n0=12, take_until_maxpvar_below=.9,
-#'     package="GauPro", design='sFFLHD', selection_method="max_des_red")
-#' a$run(5)
+#'     package="GauPro", design='sFFLHD', selection_method="max_des_red",
+#'     alpha_des=1)
+#' a$run(2)
+#'
+#'
+#' # limnonpoly, grad_norm2_mean, laGP_GauPro_kernel
+#' set.seed(1); csa(); a <- adapt.concept2.sFFLHD.R6$new(
+#'   D=2,L=3,func=limnonpoly, nugget = 1e-7,estimate.nugget = T,
+#'   obj="desirability", des_func=des_func_grad_norm2_mean,
+#'   actual_des_func=NULL,#get_num_actual_des_func_grad_norm2_mean(),
+#'   stage1batches=2, alpha_des=1, weight_const=0,
+#'   package="laGP_GauPro_kernel", design='sFFLHD',
+#'   error_power=2,
+#'   selection_method="max_des_red_all_best"
+#'   # selection_method="ALC_all_best"
+#' ); a$run(1)
+#' a$run(2)
+#'
 #' @field X Design matrix
 #' @field Z Responses
 #' @field b batch size
