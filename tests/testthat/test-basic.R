5b0b25537287bbe18cf3b9c4cdb2f89a0cb1b83b
Collin Erickson <collinberickson@gmail.com>
2020-01-25 15:24:14 -0500

    adding testthat

---
+++ tests/testthat/test-basic.R
@@ -0,0 +1,9 @@
+test_that("multiplication works", {
+  a <- adapt.concept2.sFFLHD.R6$new(D=2,L=3,func=TestFunctions::gaussian1,obj="desirability",
+                                    des_func=des_func_relmax, n0=12, take_until_maxpvar_below=.9,
+                                    package="GauPro", design='sFFLHD', selection_method="max_des_red",
+                                    alpha_des=1)
+  a$run(2)
+  expect_is(a, "adapt.concept2.sFFLHD.seq")
+  expect_is(a, "R6")
+})
