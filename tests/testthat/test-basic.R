673c1e3bb664f32ab8ac29f2177b2dbd3d6e0cce
Collin Erickson <collinberickson@gmail.com>
2020-01-25 16:19:52 -0500

    documentation changes

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -1,6 +1,6 @@
 test_that("multiplication works", {
   a <- adapt.concept2.sFFLHD.R6$new(D=2,L=3,func=TestFunctions::gaussian1,obj="desirability",
-                                    des_func=des_func_relmax, n0=12, take_until_maxpvar_below=.9,
+                                    des_func=des_func_relmax, n0=20, take_until_maxpvar_below=.9,
                                     package="GauPro", design='sFFLHD', selection_method="max_des_red",
                                     alpha_des=1)
   a$run(2)
