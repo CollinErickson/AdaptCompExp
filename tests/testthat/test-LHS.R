2fca16cab4516622b936ef44880a6e235db911e6
Collin Erickson <collinberickson@gmail.com>
2020-01-31 10:46:15 -0500

    adding test

--- tests/testthat/test-LHS.R
+++ tests/testthat/test-LHS.R
@@ -1,7 +1,11 @@
-test_that("multiplication works", {
+test_that("LHS", {
   a <- simple.LHS(n=10, d=3)
   expect_is(a, 'matrix')
   expect_equal(nrow(a), 10)
-  expect_equal(ncol(a), d)
+  expect_equal(ncol(a), 3)
   expect_true(is.LHS(a))
+
+  g <- simple.grid(n=10, d=3)
+  expect_true(is.matrix(g))
+  expect_equal(dim(g), c(1000, 3))
 })
