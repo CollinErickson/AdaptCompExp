27a44cafa377da4f4b826f6320cc32d7427b5bd7
Collin Erickson <collinberickson@gmail.com>
2020-03-14 12:01:14 -0400

    lhs test

--- tests/testthat/test-LHS.R
+++ tests/testthat/test-LHS.R
@@ -5,6 +5,14 @@ test_that("LHS", {
   expect_equal(ncol(a), 3)
   expect_true(is.LHS(a))
 
+  a <- simple.LHS(n=10, d=3, centered=TRUE)
+  expect_is(a, 'matrix')
+  expect_equal(nrow(a), 10)
+  expect_equal(ncol(a), 3)
+  expect_true(is.LHS(a))
+
+
+
   g <- simple.grid(n=10, d=3)
   expect_true(is.matrix(g))
   expect_equal(dim(g), c(1000, 3))
